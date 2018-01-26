eb5f80a9beecd4b394298c8cf7e55cd6ec64910a
Slavomir Dittrich <slavomir.dittrich@nordcloud.com>
2018-01-26 13:25:06 +0100

    Edited dummy.py.

--- tropo/dummy.py
+++ tropo/dummy.py
@@ -3,13 +3,29 @@ from uuid import uuid4
 
 t = Template()
 
+# def update_dummy_wch(template):
+#     template.add_resource(cloudformation.WaitConditionHandle(
+#         str(uuid4()).replace("-", "")
+#     ))
+#
+#
+# update_dummy_wch(t)
 
-def update_dummy_wch(template):
-    template.add_resource(cloudformation.WaitConditionHandle(
-        str(uuid4()).replace("-", "")
-    ))
 
+dummy_type_parameter = t.add_parameter(Parameter(
+    "DummyTypeParameter",
+    Type="String",
+    Description="Type of dummy stack [UAT, PROD] from config-uat.json and config-prod.json files."
+))
 
-update_dummy_wch(t)
+dummy_resource = t.add_resource(cloudformation.WaitConditionHandle(
+    str(uuid4()).replace("-", "")
+))
+
+t.add_output(Output(
+    "DummyResourceOutput",
+    Description="Dummy resource name",
+    Value=Ref(dummy_resource),
+))
 
 print(t.to_json())
